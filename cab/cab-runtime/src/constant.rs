0b0dd2fad64cac36738fe19058836e5c91a9b5c3
RGBCube <git@rgbcu.be>
2025-04-23 00:11:14 +0300

    feat: more work on the compiler

--- cab/cab-runtime/src/constant.rs
+++ cab/cab-runtime/src/constant.rs
@@ -1,5 +1,8 @@
 use std::sync::Arc;
 
+use indexmap::IndexMap;
+use rustc_hash::FxHasher;
+
 use crate::Code;
 
 #[warn(variant_size_differences)]
@@ -7,9 +10,13 @@ use crate::Code;
 pub enum Constant {
    Nil,
 
+   Path(String),
+
    Rune(char),
    Integer(num::BigInt),
    Float(f64),
+
+   Attributes(Arc<IndexMap<String, Constant, FxHasher>>),
 
    Blueprint(Arc<Code>),
 }
