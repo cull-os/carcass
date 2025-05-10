0f62b8c264abacdb76e579b16486ec562c6da0fb
RGBCube <git@rgbcu.be>
2025-05-11 01:06:38 +0300

    feat: fix ust

--- ust/src/lib.rs
+++ ust/src/lib.rs
@@ -192,9 +192,111 @@ pub struct Styled<T> {
    pub style: Style,
 }
 
+macro_rules! styled {
+   ($($method:ident),* $(,)?) => {
+      $(
+         #[must_use]
+         pub fn $method(mut self) -> Self {
+            self.style = self.style.$method();
+            self
+         }
+      )*
+   };
+}
+
 impl<T> Styled<T> {
    pub fn into_inner(self) -> T {
       self.value
+   }
+
+   #[must_use]
+   pub fn fg(mut self, color: Color) -> Self {
+      self.style = self.style.fg(color);
+      self
+   }
+
+   #[must_use]
+   pub fn bg(mut self, color: Color) -> Self {
+      self.style = self.style.bg(color);
+      self
+   }
+
+   #[must_use]
+   pub fn attr(mut self, attrs: impl Into<EnumSet<Attr>>) -> Self {
+      self.style = self.style.attr(attrs);
+      self
+   }
+
+   #[must_use]
+   pub fn fixed(mut self, color: u8) -> Self {
+      self.style = self.style.fixed(color);
+      self
+   }
+
+   #[must_use]
+   pub fn on_fixed(mut self, color: u8) -> Self {
+      self.style = self.style.on_fixed(color);
+      self
+   }
+
+   #[must_use]
+   pub fn rgb(mut self, r: u8, b: u8, g: u8) -> Self {
+      self.style = self.style.rgb(r, g, b);
+      self
+   }
+
+   #[must_use]
+   pub fn on_rgb(mut self, r: u8, b: u8, g: u8) -> Self {
+      self.style = self.style.on_rgb(r, g, b);
+      self
+   }
+
+   styled! {
+      black,
+      red,
+      green,
+      yellow,
+      blue,
+      magenta,
+      cyan,
+      white,
+
+      on_black,
+      on_red,
+      on_green,
+      on_yellow,
+      on_blue,
+      on_magenta,
+      on_cyan,
+      on_white,
+
+      bright_black,
+      bright_red,
+      bright_green,
+      bright_yellow,
+      bright_blue,
+      bright_magenta,
+      bright_cyan,
+      bright_white,
+
+      on_bright_black,
+      on_bright_red,
+      on_bright_green,
+      on_bright_yellow,
+      on_bright_blue,
+      on_bright_magenta,
+      on_bright_cyan,
+      on_bright_white,
+
+      bold,
+      dim,
+      italic,
+      underline,
+      blink,
+      rapid_blink,
+      invert,
+      conceal,
+      strike,
    }
 }
 
@@ -322,238 +424,235 @@ where
 
 impl<T> StyledExt for T {}
 
-pub trait DisplayStyled {
-   fn display_styled(&self, w: &mut dyn WriteStyled) -> io::Result<()>;
+pub trait DisplayStyled<W: WriteStyled> {
+   fn display_styled(&self, w: &mut W) -> io::Result<()>;
 }
 
 pub trait WriteStyled: io::Write {
-   fn write_styled(&mut self, value: Styled<&mut dyn fmt::Display>) -> io::Result<()>;
+   fn write_styled<T: fmt::Display>(&mut self, value: &Styled<T>) -> io::Result<()>;
 
    fn finish(&mut self) -> io::Result<()>;
+}
 
-   fn terminal(inner: impl io::Write) -> impl WriteStyled
-   where
-      Self: Sized,
-   {
-      const RESET: &[u8] = b"\x1B[0m";
+pub fn terminal(inner: impl io::Write) -> impl WriteStyled {
+   const RESET: &[u8] = b"\x1B[0m";
 
-      #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-      enum Variant {
-         Fg,
-         Bg,
+   #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+   enum Variant {
+      Fg,
+      Bg,
+   }
+
+   fn fg(color: Color) -> &'static [u8] {
+      match color {
+         Color::Primary => b"39",
+         Color::Fixed(_) | Color::Rgb(..) => b"38",
+         Color::Black => b"30",
+         Color::Red => b"31",
+         Color::Green => b"32",
+         Color::Yellow => b"33",
+         Color::Blue => b"34",
+         Color::Magenta => b"35",
+         Color::Cyan => b"36",
+         Color::White => b"37",
+         Color::BrightBlack => b"90",
+         Color::BrightRed => b"91",
+         Color::BrightGreen => b"92",
+         Color::BrightYellow => b"93",
+         Color::BrightBlue => b"94",
+         Color::BrightMagenta => b"95",
+         Color::BrightCyan => b"96",
+         Color::BrightWhite => b"97",
+      }
+   }
+
+   fn bg(color: Color) -> &'static [u8] {
+      match color {
+         Color::Primary => b"49",
+         Color::Fixed(_) | Color::Rgb(..) => b"48",
+         Color::Black => b"40",
+         Color::Red => b"41",
+         Color::Green => b"42",
+         Color::Yellow => b"43",
+         Color::Blue => b"44",
+         Color::Magenta => b"45",
+         Color::Cyan => b"46",
+         Color::White => b"47",
+         Color::BrightBlack => b"100",
+         Color::BrightRed => b"101",
+         Color::BrightGreen => b"102",
+         Color::BrightYellow => b"103",
+         Color::BrightBlue => b"104",
+         Color::BrightMagenta => b"105",
+         Color::BrightCyan => b"106",
+         Color::BrightWhite => b"107",
+      }
+   }
+
+   fn attr(attr: Attr) -> &'static [u8] {
+      match attr {
+         Attr::Bold => b"1",
+         Attr::Dim => b"2",
+         Attr::Italic => b"3",
+         Attr::Underline => b"4",
+         Attr::Blink => b"5",
+         Attr::RapidBlink => b"6",
+         Attr::Invert => b"7",
+         Attr::Conceal => b"8",
+         Attr::Strike => b"9",
+      }
+   }
+
+   fn unattr(attr: Attr) -> &'static [u8] {
+      match attr {
+         Attr::Bold => b"22",
+         Attr::Dim => b"22",
+         Attr::Italic => b"23",
+         Attr::Underline => b"24",
+         Attr::Blink => b"25",
+         Attr::RapidBlink => b"25",
+         Attr::Invert => b"27",
+         Attr::Conceal => b"28",
+         Attr::Strike => b"29",
+      }
+   }
+
+   fn write_color_start(
+      writer: &mut impl io::Write,
+      color: Color,
+      variant: Variant,
+   ) -> io::Result<()> {
+      writer.write_all(match variant {
+         Variant::Fg => fg(color),
+         Variant::Bg => bg(color),
+      })?;
+
+      match color {
+         Color::Fixed(num) => {
+            let mut buffer = itoa::Buffer::new();
+
+            writer.write_all(b";5;")?;
+            writer.write_all(buffer.format(num).as_bytes())
+         },
+
+         Color::Rgb(r, g, b) => {
+            let mut buffer = itoa::Buffer::new();
+
+            writer.write_all(b";2;")?;
+            writer.write_all(buffer.format(r).as_bytes())?;
+            writer.write_all(b";")?;
+            writer.write_all(buffer.format(g).as_bytes())?;
+            writer.write_all(b";")?;
+            writer.write_all(buffer.format(b).as_bytes())
+         },
+
+         _ => Ok(()),
+      }
+   }
+
+   struct TerminalWriter<W: io::Write> {
+      inner:   W,
+      style:   Style,
+      written: bool,
+   }
+
+   impl<W: io::Write> io::Write for TerminalWriter<W> {
+      fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+         self.inner.write_all(RESET)?;
+         self.style = Style::default();
+
+         self.inner.write(buf)
       }
 
-      fn fg(color: Color) -> &'static [u8] {
-         match color {
-            Color::Primary => b"39",
-            Color::Fixed(_) | Color::Rgb(..) => b"38",
-            Color::Black => b"30",
-            Color::Red => b"31",
-            Color::Green => b"32",
-            Color::Yellow => b"33",
-            Color::Blue => b"34",
-            Color::Magenta => b"35",
-            Color::Cyan => b"36",
-            Color::White => b"37",
-            Color::BrightBlack => b"90",
-            Color::BrightRed => b"91",
-            Color::BrightGreen => b"92",
-            Color::BrightYellow => b"93",
-            Color::BrightBlue => b"94",
-            Color::BrightMagenta => b"95",
-            Color::BrightCyan => b"96",
-            Color::BrightWhite => b"97",
+      fn flush(&mut self) -> io::Result<()> {
+         self.inner.flush()
+      }
+   }
+
+   impl<W: io::Write> WriteStyled for TerminalWriter<W> {
+      fn write_styled<T: fmt::Display>(&mut self, styled: &Styled<T>) -> io::Result<()> {
+         struct Splicer {
+            written: bool,
          }
-      }
 
-      fn bg(color: Color) -> &'static [u8] {
-         match color {
-            Color::Primary => b"49",
-            Color::Fixed(_) | Color::Rgb(..) => b"48",
-            Color::Black => b"40",
-            Color::Red => b"41",
-            Color::Green => b"42",
-            Color::Yellow => b"43",
-            Color::Blue => b"44",
-            Color::Magenta => b"45",
-            Color::Cyan => b"46",
-            Color::White => b"47",
-            Color::BrightBlack => b"100",
-            Color::BrightRed => b"101",
-            Color::BrightGreen => b"102",
-            Color::BrightYellow => b"103",
-            Color::BrightBlue => b"104",
-            Color::BrightMagenta => b"105",
-            Color::BrightCyan => b"106",
-            Color::BrightWhite => b"107",
-         }
-      }
-
-      fn attr(attr: Attr) -> &'static [u8] {
-         match attr {
-            Attr::Bold => b"1",
-            Attr::Dim => b"2",
-            Attr::Italic => b"3",
-            Attr::Underline => b"4",
-            Attr::Blink => b"5",
-            Attr::RapidBlink => b"6",
-            Attr::Invert => b"7",
-            Attr::Conceal => b"8",
-            Attr::Strike => b"9",
-         }
-      }
-
-      fn unattr(attr: Attr) -> &'static [u8] {
-         match attr {
-            Attr::Bold => b"22",
-            Attr::Dim => b"22",
-            Attr::Italic => b"23",
-            Attr::Underline => b"24",
-            Attr::Blink => b"25",
-            Attr::RapidBlink => b"25",
-            Attr::Invert => b"27",
-            Attr::Conceal => b"28",
-            Attr::Strike => b"29",
-         }
-      }
-
-      fn write_color_start(
-         writer: &mut impl io::Write,
-         color: Color,
-         variant: Variant,
-      ) -> io::Result<()> {
-         writer.write_all(match variant {
-            Variant::Fg => fg(color),
-            Variant::Bg => bg(color),
-         })?;
-
-         match color {
-            Color::Fixed(num) => {
-               let mut buffer = itoa::Buffer::new();
-
-               writer.write_all(b";5;")?;
-               writer.write_all(buffer.format(num).as_bytes())
-            },
-
-            Color::Rgb(r, g, b) => {
-               let mut buffer = itoa::Buffer::new();
-
-               writer.write_all(b";2;")?;
-               writer.write_all(buffer.format(r).as_bytes())?;
-               writer.write_all(b";")?;
-               writer.write_all(buffer.format(g).as_bytes())?;
-               writer.write_all(b";")?;
-               writer.write_all(buffer.format(b).as_bytes())
-            },
-
-            _ => Ok(()),
-         }
-      }
-
-      struct TerminalWriter<W: io::Write> {
-         inner:   W,
-         style:   Style,
-         written: bool,
-      }
-
-      impl<W: io::Write> io::Write for TerminalWriter<W> {
-         fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-            self.inner.write_all(RESET)?;
-            self.style = Style::default();
-
-            self.inner.write(buf)
-         }
-
-         fn flush(&mut self) -> io::Result<()> {
-            self.inner.flush()
-         }
-      }
-
-      impl<W: io::Write> WriteStyled for TerminalWriter<W> {
-         fn write_styled(&mut self, styled: Styled<&mut dyn fmt::Display>) -> io::Result<()> {
-            struct Splicer {
-               written: bool,
-            }
-
-            impl Splicer {
-               fn splice(&mut self, writer: &mut impl io::Write) -> io::Result<()> {
-                  if self.written {
-                     writer.write_all(b";")
-                  } else {
-                     self.written = true;
-                     writer.write_all(b"\x1B[")
-                  }
-               }
-
-               fn finish(self, writer: &mut impl io::Write) -> io::Result<()> {
-                  if self.written {
-                     writer.write_all(b"m")
-                  } else {
-                     Ok(())
-                  }
+         impl Splicer {
+            fn splice(&mut self, writer: &mut impl io::Write) -> io::Result<()> {
+               if self.written {
+                  writer.write_all(b";")
+               } else {
+                  self.written = true;
+                  writer.write_all(b"\x1B[")
                }
             }
 
-            let Style {
-               fg: fg_old,
-               bg: bg_old,
-               attrs: attrs_old,
-            } = self.style;
-
-            let Style {
-               fg: fg_new,
-               bg: bg_new,
-               attrs: attrs_new,
-            } = styled.style;
-
-            let mut splicer = Splicer { written: false };
-
-            if fg_old != fg_new {
-               self.style.fg = fg_new;
-
-               splicer.splice(&mut self.inner)?;
-               write_color_start(&mut self.inner, fg_new, Variant::Fg)?;
-            }
-
-            if bg_old != bg_new {
-               self.style.bg = bg_new;
-
-               splicer.splice(&mut self.inner)?;
-               write_color_start(&mut self.inner, bg_new, Variant::Bg)?;
-            }
-
-            for attr_deleted in attrs_old.difference(attrs_new) {
-               splicer.splice(&mut self.inner)?;
-               self.inner.write_all(unattr(attr_deleted))?;
-            }
-
-            for attr_added in attrs_new.difference(attrs_old) {
-               splicer.splice(&mut self.inner)?;
-               self.inner.write_all(attr(attr_added))?;
-            }
-
-            if splicer.written {
-               self.written = true;
-            }
-
-            splicer.finish(&mut self.inner)?;
-
-            write!(self.inner, "{value}", value = styled.value)
-         }
-
-         fn finish(&mut self) -> io::Result<()> {
-            if self.written {
-               self.inner.write_all(RESET)
-            } else {
-               Ok(())
+            fn finish(self, writer: &mut impl io::Write) -> io::Result<()> {
+               if self.written {
+                  writer.write_all(b"m")
+               } else {
+                  Ok(())
+               }
             }
          }
+
+         let Style {
+            fg: fg_old,
+            bg: bg_old,
+            attrs: attrs_old,
+         } = self.style;
+
+         let Style {
+            fg: fg_new,
+            bg: bg_new,
+            attrs: attrs_new,
+         } = styled.style;
+
+         let mut splicer = Splicer { written: false };
+
+         if fg_old != fg_new {
+            self.style.fg = fg_new;
+
+            splicer.splice(&mut self.inner)?;
+            write_color_start(&mut self.inner, fg_new, Variant::Fg)?;
+         }
+
+         if bg_old != bg_new {
+            self.style.bg = bg_new;
+
+            splicer.splice(&mut self.inner)?;
+            write_color_start(&mut self.inner, bg_new, Variant::Bg)?;
+         }
+
+         for attr_deleted in attrs_old.difference(attrs_new) {
+            splicer.splice(&mut self.inner)?;
+            self.inner.write_all(unattr(attr_deleted))?;
+         }
+
+         for attr_added in attrs_new.difference(attrs_old) {
+            splicer.splice(&mut self.inner)?;
+            self.inner.write_all(attr(attr_added))?;
+         }
+
+         if splicer.written {
+            self.written = true;
+         }
+
+         splicer.finish(&mut self.inner)?;
+
+         write!(self.inner, "{value}", value = styled.value)
       }
 
-      TerminalWriter {
-         inner,
-         style: Style::default(),
-         written: false,
+      fn finish(&mut self) -> io::Result<()> {
+         if self.written {
+            self.inner.write_all(RESET)
+         } else {
+            Ok(())
+         }
       }
+   }
+
+   TerminalWriter {
+      inner,
+      style: Style::default(),
+      written: false,
    }
 }
