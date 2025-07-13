b49d2aead6db556165a7191b0f5f72873dd1d22d
RGBCube <git@rgbcu.be>
2025-07-14 00:51:43 +0300

    con.interface: cleanup

--- con/interface.rs
+++ con/interface.rs
@@ -1,19 +1,17 @@
-use std::{
-   io,
-   net,
-   pin::Pin,
-   task,
-};
+use std::net;
 
 use cyn::ResultExt as _;
-use tokio::io::{
-   AsyncRead,
-   AsyncWrite,
-   ReadBuf,
+use derive_more::{
+   Deref,
+   DerefMut,
 };
 
+#[derive(Deref, DerefMut)]
 pub struct Interface {
+   #[deref]
+   #[deref_mut]
    device: tun::AsyncDevice,
+
    pub v4: net::Ipv4Addr,
    pub v6: net::Ipv6Addr,
 }
@@ -38,39 +36,5 @@ impl Interface {
       tracing::info!("Created TUN interface '{name}' with IPv4 {v4} and IPv6 {v6}.");
 
       Ok(Self { device, v4, v6 })
-   }
-}
-
-impl AsyncRead for Interface {
-   fn poll_read(
-      mut self: Pin<&mut Self>,
-      context: &mut task::Context<'_>,
-      buffer: &mut ReadBuf<'_>,
-   ) -> task::Poll<io::Result<()>> {
-      Pin::new(&mut self.device).poll_read(context, buffer)
-   }
-}
-
-impl AsyncWrite for Interface {
-   fn poll_write(
-      mut self: Pin<&mut Self>,
-      context: &mut task::Context<'_>,
-      buffer: &[u8],
-   ) -> task::Poll<Result<usize, io::Error>> {
-      Pin::new(&mut self.device).poll_write(context, buffer)
-   }
-
-   fn poll_flush(
-      mut self: Pin<&mut Self>,
-      context: &mut task::Context<'_>,
-   ) -> task::Poll<Result<(), io::Error>> {
-      Pin::new(&mut self.device).poll_flush(context)
-   }
-
-   fn poll_shutdown(
-      mut self: Pin<&mut Self>,
-      context: &mut task::Context<'_>,
-   ) -> task::Poll<Result<(), io::Error>> {
-      Pin::new(&mut self.device).poll_shutdown(context)
    }
 }
