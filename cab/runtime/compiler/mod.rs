c21c4718548f4c3d00197eb8302242f17edacaf7
RGBCube <git@rgbcu.be>
2025-11-26 21:54:27 +0300

    cab.runtime.compiler: add an ImplicitApply and Apply branch next to pipe

--- cab/runtime/compiler/mod.rs
+++ cab/runtime/compiler/mod.rs
@@ -372,6 +372,10 @@ impl<'a> Emitter<'a> {
                   return;
                },
 
+               node::InfixOperator::ImplicitApply | node::InfixOperator::Apply => {
+                  this.emit(left);
+                  this.emit(right);
+               },
                node::InfixOperator::Pipe => {
                   this.emit(right);
                   this.emit(left);
